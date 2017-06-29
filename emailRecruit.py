6a344a5ed2505afa95441b01b70af0199d1b4a06
Danis Fermi <danisfermijohn@gmail.com>
2017-06-29 22:03:12 +0530

    Working Checkpoint. Send Email

--- emailRecruit.py
+++ emailRecruit.py
@@ -1,2 +1,35 @@
 #!/usr/bin/python
-print "Hello World"
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
+
+fromaddr = "danisfermijohn@gmail.com"
+toaddr = "dfermi@ncsu.edu"
+
+msg = MIMEMultipart()
+
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "Fall 2017 Full Time Application"
+
+body = "Hello"
+msg.attach(MIMEText(body, 'plain'))
+
+filename = "resume.pdf"
+attachment = open("resume.pdf", "rb")
+ 
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+ 
+msg.attach(part)
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "YOUR PASSWORD")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
