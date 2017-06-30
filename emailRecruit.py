d287b22245f796b86f83e5edc30e64d0a7ae5a9d
Danis Fermi <danisfermijohn@gmail.com>
2017-06-30 17:39:00 +0530

    Working Checkpoint. Read and Parse from Excel

--- emailRecruit.py
+++ emailRecruit.py
@@ -4,6 +4,29 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
 from email import encoders
+import xlrd
+
+# Class to hold Recruiter Object
+# Recruiter Object has following attributes:-
+class Recruiter(object):
+    def __init__(self, firstName, lastName, company, mailID, position, jobID):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.company = company
+        self.mailID = mailID
+        self.position = position
+        self.jobID = jobID
+
+    def __str__(self):
+        return("Recruiter object:\n"
+               "  firstName = {0}\n"
+               "  lastName = {1}\n"
+               "  Company = {2}\n"
+               "  mailID = {3}\n"
+               "  position = {4} \n"
+               "  jobID = {5}"
+               .format(self.firstName, self.lastName, self.company,
+                       self.mailID, self.position, self.jobID))
 
 # Function to read template mail from mailTemplate and replace generic strings with Company specific values
 # Takes Name, Company, Position as arguments
@@ -19,30 +42,54 @@ def getBody(name, company, position):
         return "Hello"
 
 fromaddr = "danisfermijohn@gmail.com"
-toaddr = "dfermi@ncsu.edu"
 
 msg = MIMEMultipart()
-
 msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = "Fall 2017 Full Time Application"
 
-body = getBody("Name","Company","Position")
-msg.attach(MIMEText(body, 'plain'))
+# Login to Gmail
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "YOUR PASSWORD") # Replace with your password here
 
-filename = "resume.pdf"
-attachment = open("resume.pdf", "rb")
- 
+# Read from and Attach Resume to Mail
+filename = "DanisFermiResume.pdf"
+attachment = open(filename, "rb")
 part = MIMEBase('application', 'octet-stream')
 part.set_payload((attachment).read())
 encoders.encode_base64(part)
 part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
- 
 msg.attach(part)
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(fromaddr, "YOUR PASSWORD")
-text = msg.as_string()
-server.sendmail(fromaddr, toaddr, text)
+# Opening and Parsing Excel data into list of Recruiter Objects
+wb = xlrd.open_workbook("recruiters.xlsx")
+for sheet in wb.sheets():
+    number_of_rows = sheet.nrows
+    number_of_columns = sheet.ncols
+    recruiters = []
+
+    rows = []
+    for row in range(1, number_of_rows):
+        values = []
+        for col in range(number_of_columns):
+            value  = (sheet.cell(row,col).value)
+            try:
+                value = str(int(value))
+            except ValueError:
+                pass
+            finally:
+                values.append(value)
+        recruiter = Recruiter(*values)
+        recruiters.append(recruiter)
+
+for recruiter in recruiters:
+    msg['To'] = recruiter.mailID
+    msg['Subject'] = "Danis Fermi-MS Student-Job Application for $insertPosition$ position".replace("$insertPosition$", recruiter.position)
+    if recruiter.jobID != "":
+        msg['Subject'].append("(Job Id: "+recruiter.jobID+")")
+
+    body = getBody(recruiter.firstName, recruiter.company, recruiter.position)
+    msg.attach(MIMEText(body, 'plain'))
+
+    text = msg.as_string()
+    server.sendmail(fromaddr, recruiter.mailID, text)
 server.quit()
